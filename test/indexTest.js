0dc28a3f5965390eda1fa38718af15478b2135e6
Stefanos <stefanosu422@gmail.com>
2019-08-27 14:16:55 -0400

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,9 +1,9 @@
 const expect = chai.expect;
 
-describe('index.js', function () {
-  describe('displayTime()', function () {
-    it('can successfully access moment() from the momentjs node module', function () {
-      expect(moment()).to.exist
+describe('index.js', function() {
+    describe('displayTime()', function() {
+        it('can successfully access moment() from the momentjs node module', function() {
+            expect(moment()).to.exist
+        });
     });
-  });
 });
